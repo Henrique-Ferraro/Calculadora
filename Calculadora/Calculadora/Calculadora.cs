b458ec8e5a89a91c926858769a0dd22f482e227a
Henrique-Ferraro <65721269+Henrique-Ferraro@users.noreply.github.com>
2023-09-12 18:39:46 -0300

    Add files via upload

---
+++ Calculadora/Calculadora/Calculadora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Calculadora
+{
+    public class Calculadora
+    {
+        
+        public Operacoes calcular(Operacoes operacao)
+        {
+            //int resultado = 0; //incluindo
+
+            switch(operacao.operador)
+            {
+                case '+': operacao.resultado = soma(operacao); break;
+                case '-': operacao.resultado = subtracao(operacao); break;
+                case '*': operacao.resultado = multiplicacao(operacao); break;
+                case '/': operacao.resultado = divisao(operacao); break;
+                default: operacao.resultado = 0; break;
+            }
+            return operacao;
+        }
+        public long soma(Operacoes operacao)
+        {
+            return operacao.valorA + operacao.valorB;
+        }
+        public long subtracao(Operacoes operacao)
+        {
+            return operacao.valorA - operacao.valorB;
+        }
+        public long multiplicacao(Operacoes operacao)
+        {
+            return operacao.valorA * operacao.valorB;
+        }
+        public decimal divisao(Operacoes operacao)
+        {
+            return operacao.valorA / operacao.valorB;
+        }
+
+    }
+}
