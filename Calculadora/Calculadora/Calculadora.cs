8dd82e17d44277b85fbd0943f441aea318b2f307
Henrique-Ferraro <65721269+Henrique-Ferraro@users.noreply.github.com>
2023-09-13 19:37:53 -0300

    Update Calculadora.cs

--- Calculadora/Calculadora/Calculadora.cs
+++ Calculadora/Calculadora/Calculadora.cs
@@ -12,24 +12,24 @@ namespace Calculadora
                 case '+': operacao.resultado = soma(operacao); break;
                 case '-': operacao.resultado = subtracao(operacao); break;
                 case '*': operacao.resultado = multiplicacao(operacao); break;
-                case '/': operacao.resultado = divisao(operacao); break;
+                case '/': operacao.resultado = divisao(operacao); break;  //Implementação da operação divisão
                 default: operacao.resultado = 0; break;
             }
             return operacao;
         }
-        public long soma(Operacoes operacao)
+        public long soma(Operacoes operacao)  //Alteração dos tipos de dados de int para long
         {
             return operacao.valorA + operacao.valorB;
         }
-        public long subtracao(Operacoes operacao)
+        public long subtracao(Operacoes operacao)  //Alteração dos tipos de dados de int para long
         {
             return operacao.valorA - operacao.valorB;
         }
-        public long multiplicacao(Operacoes operacao)
+        public long multiplicacao(Operacoes operacao)  //Alteração dos tipos de dados de int para long
         {
             return operacao.valorA * operacao.valorB;
         }
-        public decimal divisao(Operacoes operacao)
+        public decimal divisao(Operacoes operacao)  //Inclusão do metodo divisão
         {
             return operacao.valorA / operacao.valorB;
         }
