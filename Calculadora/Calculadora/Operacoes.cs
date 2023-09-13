73f934f02bcadd130677af92c6b83b2bd95af882
Henrique-Ferraro <65721269+Henrique-Ferraro@users.noreply.github.com>
2023-09-13 19:39:17 -0300

    Update Operacoes.cs

--- Calculadora/Calculadora/Operacoes.cs
+++ Calculadora/Calculadora/Operacoes.cs
@@ -3,9 +3,9 @@ namespace Calculadora
 {
     public class Operacoes
     {
-        public long valorA {get;set;}
+        public long valorA {get;set;} //Alteração dos tipos de dados de int para long
         public char operador { get; set; }
-        public long valorB { get; set; }
+        public long valorB { get; set; } //Alteração dos tipos de dados de int para long
         public decimal resultado { get; set; }
 
     }
